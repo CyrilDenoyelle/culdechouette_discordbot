dbd65d025d901d899f34f277cd1e8b2b3cf20194
CyrilDenoyelle <cyril-denoyelle@hotmail.fr>
2020-11-21 03:04:36 +0100

    functionnal on ready messages

--- bot.js
+++ bot.js
@@ -27,15 +27,19 @@ client.on('ready', () => {
   process.env.SELF_ID = client.user.id;
 
   console.log(`Logged in as ${client.user.tag}! id: ${process.env.SELF_ID}`);
-  client.users.cache.get(process.env.ADMIN) ? console.log("LAAAA J'Y SUIS PUTAAAAAIN") : null;
+
+  // to stop spam durring dev: comment from here -------------
+  const defaultGuild = client.guilds.cache.find(guild => guild.id === process.env.DEFAULT_GUILD);
+  defaultGuild.members.fetch(process.env.ADMIN)
+    .then(user => user.createDM()) // create direct message channel with the user
+    .then(dmChannel => dmChannel.send('Bonjour je suis UP et prèt a développer...')) // then send a message to the user
+    .catch(console.error);
 
   client.channels.cache.get(process.env.UP_CHANNEL) // get channel with id = process.env.UP_CHANNEL
     .send('UP UP UP !') // post a message on founded channel
     .then(e => e.delete({ timeout: 5000 })); // delete message after 5 secs
+  // to here -------------
 
-  // client.users.cache.get(process.env.ADMIN) // get user with id = process.env.ADMIN
-  //   .createDM() // create direct message channel with the user
-  //   .then(e => e.send('Bonjour je suis UP et prèt a développer...')); // then send a message to the user
 });
 
 client.on('message', (msg) => { // on message received in any channel where the bot is
