73360cfc9ea9af239f6668d3a8b52992b306b106
CyrilDenoyelle <cyril-denoyelle@hotmail.fr>
2020-12-05 00:42:10 +0100

    remove semicolon...

--- helpers/culDeChouette/rules.js
+++ helpers/culDeChouette/rules.js
@@ -4,5 +4,5 @@ module.export = {
   culDeChouette: dices => dices[0] === dices[1] && dices[1] === dices[2],
 
   // Chouette Rule: if 2 dices are equals
-  chouette: dices => dices[0] === dices[1] || dices[1] === dices[2] || dices[0] === dices[2];
+  chouette: dices => dices[0] === dices[1] || dices[1] === dices[2] || dices[0] === dices[2]
 }
