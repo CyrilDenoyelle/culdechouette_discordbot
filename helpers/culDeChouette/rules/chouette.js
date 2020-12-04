99f8187efb15643a0fe1e661502a9d6bdcf14b7b
CyrilDenoyelle <cyril-denoyelle@hotmail.fr>
2020-12-05 00:36:17 +0100

    add rules folder + cul de chouette and chouette

---
+++ helpers/culDeChouette/rules/chouette.js
@@ -0,0 +1,4 @@
+
+// Chouette Rule: if 2 dices are equals
+
+module.exports = dices => dices[0] === dices[1] || dices[1] === dices[2] || dices[0] === dices[2];
---
+++ helpers/culDeChouette/rules/culDeChouette.js
@@ -0,0 +1,4 @@
+
+// Regle Cul de chouette Rule: if all the dices are equals
+
+module.exports = dices => dices[0] === dices[1] && dices[1] === dices[2];
